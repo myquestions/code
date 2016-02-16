f960cf1046c12098e03b4052262922d50ed8caa3
Andrius <andrius.kb.developer@gmail.com>
2016-02-16 09:29:47 +0200

    Second update just to test.

--- mysite/myapp.py
+++ mysite/myapp.py
@@ -4,7 +4,7 @@ from bottle import default_app, route
 
 @route('/')
 def hello_world():
-    return 'Hello from My Questions! Now with changes from local env made on 2016-02-16!'
+    return 'Hello from My Questions! Now with changes from local env made on 2016-02-16! And a second update today!'
 
 application = default_app()
 
