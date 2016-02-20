49144f23f6d3f0960aa3e88efec3b61c89109561
Andrius <andrius.kb.developer@gmail.com>
2016-02-20 23:16:27 +0200

    Revert back to the python.

--- mysite/myapp.py
+++ mysite/myapp.py
@@ -1,14 +1,41 @@
-
-# A very simple Bottle Hello World app for you to get started with...
+import feedparser
 from bottle import default_app, route, static_file
+
 
 @route('/')
 def hello_world():
-    return serve_static("index.html")
+
+    main_url = "https://devquestions.wordpress.com/feed"
+
+    all_questions = []
+    
+    try_load_next = True
+    page = 1
+    
+    while try_load_next:
+
+        if page == 1:
+            url = main_url
+        else:
+            url = main_url + "/?paged=" + str(page)
+        
+        feed = feedparser.parse(url)
+
+        for post in feed.entries:
+            
+            question = post.title
+            
+            all_questions.append(question)
+            
+        page = page + 1
+        
+        try_load_next = len(feed.entries) > 0
+
+    return str(len(all_questions))
 
 @route('/static/<filename:path>')
 def serve_static(filename):
-    return static_file(filename, root="mysite/static/")
+    return static_file(filename, root="static/")
 
 application = default_app()
 
